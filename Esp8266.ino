8be5cb24ef30e1183907ed2ef801fd1eb1da4659
Youssef0095 <80364160+Youssef0095@users.noreply.github.com>
2021-03-12 15:33:17 +0100

    Update Esp8266.ino
    Updated For Power BI

--- Esp8266.ino
+++ Esp8266.ino
@@ -10,10 +10,10 @@
 
 char *ssid = "dlink-95DB";
 char *pass = "gwxyj36867";
-char *conn = "HostName=IOTHUB20-20.azure-devices.net;DeviceId=Esp8266;SharedAccessKey=BS4aYncxIIfEWvihwr5fiRdoMZwlrulpUAazgEVsJCA=";
+char *conn = "HostName=IOTHUB20-20.azure-devices.net;DeviceId=84:CC:A8:85:C2:BE;SharedAccessKey=XVGjiUP1iKFO4QShkdc96FszJeneztCgs5tGI8FNmks=";
 
 bool messagePending = false;
-int interval = 1000;
+int interval = 1000 * 6;
 unsigned long prevMillis = 0;
 time_t epochTime;
 
@@ -24,6 +24,7 @@ void setup() {
   initSerial();
   initWifi();
   initEpochTime();
+  Serial.println("\n");
   initDHT();
   initDevice();
 }
@@ -41,26 +42,22 @@ void loop() {
       prevMillis = currentMillis;
 
       epochTime = time(NULL);
-      Serial.printf("Current Time: %lu. ", epochTime);
-      Serial.println("\n");
-
       temperature = dht.readTemperature();
       humidity = dht.readHumidity();
 
-      if (!(std::isnan(temperature)) && !(std::isnan(humidity)))  {
+      if (!(std::isnan(temperature)) && !(std::isnan(humidity)) && epochTime > 28800)  {
+        
         char payload[256];
+        char epochTimeBuf[12];
 
         StaticJsonBuffer<sizeof(payload)> buf;
         JsonObject &root = buf.createObject();
-        root["type"] = "DHT";
-        root["deviceId"] = "ESP8266";
-        //root["ts"] = epochTime;
         root["temperature"] = temperature;
         root["humidity"] = humidity;
         root.printTo(payload, sizeof(payload));
 
         if (temperature > (PrevTemp + 1.0) || temperature < (PrevTemp - 1.0)) {
-          sendMessage(payload);
+          sendMessage(payload, itoa(epochTime, epochTimeBuf, 10));
           Serial.println("Message Sent...");
           PrevTemp = temperature;
         }
